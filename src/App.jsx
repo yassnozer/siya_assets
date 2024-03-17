e964db2b267db793574be2cc0c3865d57a105210
yassnozer <65410478+yassnozer@users.noreply.github.com>
2024-03-17 10:49:40 +0000

    new

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <a href="../public/serie1" className="href">
+        click
+      </a>
+      <a href="../public/serie1/question1.jpg" className="href">
+        click
+      </a>
+      <a href="../public/serie1/question1.jpg" className="href">
+        click
+      </a>
+      <a href="../public/serie1/v1.wav" className="href">
+        click
+      </a>
+    </>
+  );
+}
+
+export default App;
